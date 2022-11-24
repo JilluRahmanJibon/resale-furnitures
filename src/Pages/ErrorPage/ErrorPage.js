402749f17cb24b3748b596c4e02ef9c1810c978d
Jillu Rahman Jibon <jillurahmanjibon@gmail.com>
2022-11-24 17:31:56 +0600

    error page are handeling

--- src/Pages/ErrorPage/ErrorPage.js
+++ src/Pages/ErrorPage/ErrorPage.js
@@ -3,7 +3,6 @@ import { Link, useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
 	const errors = useRouteError()
-	console.log(errors);
 	return (
 		<div
 			className="
@@ -18,10 +17,10 @@ const ErrorPage = () => {
   ">
 			<div className="px-40 py-20 bg-white rounded-md shadow-xl">
 				<div className="flex flex-col items-center">
-					<h1 className="font-bold text-blue-600 text-9xl">404</h1>
+					<h1 className="font-bold text-blue-600 text-9xl">{errors?.status}</h1>
 
 					<h6 className="mb-2 text-2xl font-bold text-center text-gray-800 md:text-3xl">
-						<span className="text-red-500">Oops!</span> Page not found
+						<span className="text-red-500">Oops!</span> Page {errors?.statusText}
 					</h6>
 
 					<p className="mb-8 text-center text-gray-500 md:text-lg">
--- src/Pages/Furnitures/FurnitureCard.js
+++ src/Pages/Furnitures/FurnitureCard.js
@@ -3,16 +3,18 @@ import { Link } from 'react-router-dom';
 
 const FurnitureCard = ({ furniture }) => {
     console.log(furniture);
-    const { _id, name, picture } = furniture
+    const { _id, name, picture, capacity } = furniture
     return (
         <div>
             <div className=" rounded-md shadow-md dark:bg-gray-900 dark:text-gray-100">
                 <Link to={`/furnitures/${''}`}>    <img src={picture} alt="" className="object-cover object-center w-full rounded-t-md h-72 " />
                 </Link>
+                <p className='pl-2'>Capacity: {capacity}</p>
                 <div className="flex flex-col justify-between p-6 space-y-8">
                     <div className="space-y-2">
+
                         <Link to={`/furnitures/${''}`}>
-                            <h2 className="text-3xl text-center uppercase font-semibold tracking-wide">{name}</h2></Link>
+                            <h2 className="text-2xl text-center uppercase font-semibold tracking-wide">{name}</h2></Link>
 
                     </div>
 
